3d034fb0549112391187a9626183df9a779c6d26
Christopher Picolotto Rodrigues <crsprd10@hotmail.com>
2025-06-24 17:09:03 -0300

    feat: add quilometragemAtual field to vehicle forms and details, update input type and formatting

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,18 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideToastr} from 'ngx-toastr';
+import { provideToastr } from 'ngx-toastr';
 import { NgxMaskPipe } from 'ngx-mask';
-import { provideNgxMask } from 'ngx-mask'; 
+import { provideNgxMask } from 'ngx-mask';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth-interceptor';
+
+registerLocaleData(localePt);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -23,5 +27,6 @@ export const appConfig: ApplicationConfig = {
       positionClass: 'toast-bottom-right',
       preventDuplicates: true,
     }),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
   ]
 };
